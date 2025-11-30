5e3d13190a87a5a455686a2adc230634760ec061
Kiyzan <mzpnzakky@gmail.com>
2025-11-30 10:01:13 +0700

    Minor
    - Perubahan url API

--- src/components/ChatApp.vue
+++ src/components/ChatApp.vue
@@ -121,7 +121,7 @@ const token = ref(null)
 const API_BASE_URL = 'https://rinku-api.niaz.my.id'
 // const API_BASE_URL = 'http://localhost:3000'
 const LOGIN_URL = `${API_BASE_URL}/auth/login`
-const CHAT_URL = `${API_BASE_URL}/noa/chat`
+const CHAT_URL = `${API_BASE_URL}/rinku/chat`
 
 let msgId = 1
 
